084201756da2c1f22011975109c09b26b358881a
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-04-05 21:58:04 +0900

    2024-04-05
    UI 하위 아이템
    (UI_SkillSelectPopup을 이용해서 Transform을 받아와 가지고 그 밑에 있는 자식들을 다 삭제를 하는 방식도 알아 봤고Transform의 tranform은 자식위치를 나타낸다는걸 알게 되었고 그다음 UI_SkillCardItem을 추가해서 아이템을 넣어서 거기다가 Buttion이 있는 곳에다가  누르면은 스킬 레벨 업그레이드 로그도 직히고 마지막 팝업도 끄게 만들어 줬습니다.
    여기서 중요한거는 UI_SkillCardItem은 어떤 스킬인지 몇 레벨인지 데이터 시트를 가져와서 그걸 처음에 세팅을 해주고 그래야 거기에 스킬 얼굴 이름 등등이 생겨나니까 그리고 난 다음에 함수를 하나만들어서 button에다가 넣어준 겁니다.)
    UI_SkillSelectPopup
    UI_SkilLCardItem

--- Vampire Survivors/Assets/@Scripts/UI/UI_SkillCardItem.cs
+++ Vampire Survivors/Assets/@Scripts/UI/UI_SkillCardItem.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class UI_SkillCardItem : UI_Base
 {
-    // Start is called before the first frame update
-    void Start()
+    // 어떤 스킬?
+    // 몇 레벨?
+    // 데이터시트?
+    int _templateID;
+    Data.SkillData _skillData;
+
+    public void SetInfo(int templateID)
     {
-        
+        _templateID = templateID;
+
+        Managers.Data.SkillDic.TryGetValue(templateID, out _skillData);
+        // 나중에 스킬 데이터를 다 파주면 검증하는 코드가 들어가야 합니다.
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickItem()
     {
-        
+        //  스킬 레벨 업그레이드
+        Debug.Log("OnClickItem");
+        Managers.UI.ClosePopup();
     }
 }
